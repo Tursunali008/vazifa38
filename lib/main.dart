299db3ef85fe28b45e87f3a49432ab9ab222f481
Tursunali008 <153731500+Tursunali008@users.noreply.github.com>
2024-08-12 16:57:45 +0500

    ozgartirildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:vazifa38/screens/biometrik_screen.dart';
+import 'package:vazifa38/screens/pincode_screen.dart';
+import 'package:vazifa38/screens/todo_screen.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? storedPin = prefs.getString('userPin');
+  bool isBiometricEnabled = prefs.getBool('isBiometricEnabled') ?? false;
+
+  runApp(MyApp(storedPin: storedPin, isBiometricEnabled: isBiometricEnabled));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  final String? storedPin;
+  final bool isBiometricEnabled;
+
+  const MyApp({super.key, this.storedPin, required this.isBiometricEnabled});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Secure App',
+      debugShowCheckedModeBanner: false,
+      home: isBiometricEnabled
+          ? BiometricCheckPage()
+          : storedPin == null
+              ? const TodoScreen()
+              : PinEntryPage(storedPin: storedPin),
     );
   }
 }
